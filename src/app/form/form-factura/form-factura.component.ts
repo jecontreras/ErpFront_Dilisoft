3c2cb9f8f5c77c9f6eaa955fedf61daf3520e65f
jecontreras68 <joseeduar147@gmail.com>
2022-10-20 17:52:48 -0500

    cambios pred

--- src/app/form/form-factura/form-factura.component.ts
+++ src/app/form/form-factura/form-factura.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToolsService } from 'src/app/services/tools.service';
+import { ArticuloService } from 'src/app/servicesComponent/articulo.service';
 import { FacturaService } from 'src/app/servicesComponent/factura.service';
 
 @Component({
@@ -13,11 +14,25 @@ export class FormFacturaComponent implements OnInit {
   data:any = {};
   id:any;
   titleBTN:string = "Guardar";
+  tablet:any = {
+    headers:["Codigo", "Titulo", "Color", "Talla", "Cantidad"],
+    row:[],
+    keys:["codigo", "titulo","color","talla","cantidad"]
+  };
+  querys:any = {
+    where:{
+      estado:0
+    },
+    page:0,
+    limit: 10000
+  };
+  datoBusqueda:string;
 
   constructor(
     private activate: ActivatedRoute,
     private _tools: ToolsService,
-    private _factura: FacturaService
+    private _factura: FacturaService,
+    private _articulos: ArticuloService
   ) { }
 
   ngOnInit(): void {
@@ -31,6 +46,33 @@ export class FormFacturaComponent implements OnInit {
       res = res.data[0];
       this.data = res || {};
       console.log( this.data )
+    });
+  }
+  filtroGet(){
+    this.querys.where.or = [
+      {
+        slug: {
+          contains: this.datoBusqueda|| ''
+        }
+      },
+      {
+        codigo: {
+          contains: this.datoBusqueda|| ''
+        }
+      },
+      {
+        titulo: {
+          contains: this.datoBusqueda|| ''
+        }
+      }
+    ];
+    this.getArticulos();
+  }
+  getArticulos(){
+    this.tablet.row = [];
+    this._articulos.get( this.querys ).subscribe( ( res:any )=>{
+      res = res.data;
+      this.tablet.row = res;
     });
   }
 
