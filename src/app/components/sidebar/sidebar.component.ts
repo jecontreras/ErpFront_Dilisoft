33321fa7964162e051e04dec8e9e635ab76e4e52
jose contreras <joseeduar147@gmail.com>
2023-06-30 10:51:23 +0000

    cambios new plus

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,32 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { USER } from 'src/app/interfaces/sotarage';
+import { Store } from '@ngrx/store';
 
 declare interface RouteInfo {
     path: string;
     title: string;
     icon: string;
     class: string;
+    disabled?: boolean;
 }
-export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Inicio',  icon: 'ni-tv-2 text-primary', class: '' },
-    //{ path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    //{ path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'Perfil',  icon:'ni-single-02 text-yellow', class: '' },
-    //{ path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    //{ path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    //{ path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' },
-    { path: '/articulo', title: 'Articulo',  icon:'ni-briefcase-24 text-pink', class: '' },
-    { path: '/empresa', title: 'Empresa',  icon:'ni-istanbul text-pink', class: '' },
-    { path: '/factura', title: 'Factura',  icon:'ni-credit-card text-pink', class: '' },
-    { path: '/estadisticas', title: 'Estadisticas',  icon:'ni-credit-card text-pink', class: '' },
-    { path: '/actionsreturns', title: 'Devoluciones / Cambios',  icon:'ni-world text-pink', class: '' },
-    { path: '/moneypayment', title: 'Abonos dinero',  icon:'ni-credit-card text-pink', class: '' },
-    { path: '/inventario', title: 'Inventario',  icon:'ni-books text-pink', class: '' },
-    { path: '/categoria', title: 'Categoria',  icon:'ni-building text-pink', class: '' },
-    { path: '/provedor', title: 'Provedor',  icon:'ni-world text-pink', class: '' },
-    { path: '/perfil', title: 'Roles',  icon:'ni-vector text-pink', class: '' },
-    { path: '/logs', title: 'Logs',  icon:'ni-ui-04 text-pink', class: '' },
-];
+export let ROUTES: RouteInfo[] = [];
 
 @Component({
   selector: 'app-sidebar',
@@ -37,11 +21,46 @@ export class SidebarComponent implements OnInit {
 
   public menuItems: any[];
   public isCollapsed = true;
+  dataUser:any = {};
+  viewProfile:string = 'visitante';
+  constructor(
+    private router: Router,
+    private _store: Store<USER>,
+  ) {
+    this._store.subscribe((store: any) => {
+      store = store.name;
+      if(!store) return false;
+      this.dataUser = store.user || {};
+      try {
+        this.viewProfile = this.dataUser.usu_perfil.prf_descripcion;
+      } catch (error) {
 
-  constructor(private router: Router) { }
+      }
+    });
+  }
 
   ngOnInit() {
-    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    ROUTES = [
+      { path: '/dashboard', title: 'Inicio',  icon: 'ni-tv-2 text-primary', class: '' ,disabled: ( this.viewProfile === 'visitante' ) || ( this.viewProfile === 'admin' ) },
+      //{ path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
+      //{ path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
+      { path: '/user-profile', title: 'Perfil',  icon:'ni-single-02 text-yellow', class: '' , disabled: ( this.viewProfile === 'visitante' ) || ( this.viewProfile === 'admin' )},
+      //{ path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
+      //{ path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
+      //{ path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' },
+      { path: '/articulo', title: 'Articulo',  icon:'ni-briefcase-24 text-pink', class: '', disabled: ( this.viewProfile === 'admin' ) },
+      { path: '/empresa', title: 'Empresa',  icon:'ni-istanbul text-pink', class: '', disabled: ( this.viewProfile === 'admin' ) },
+      { path: '/factura', title: 'Factura',  icon:'ni-credit-card text-pink', class: '', disabled: ( this.viewProfile === 'admin' ) },
+      { path: '/estadisticas', title: 'Estadisticas',  icon:'ni-credit-card text-pink', class: '', disabled: ( this.viewProfile === 'visitante' ) || ( this.viewProfile === 'admin' ) },
+      { path: '/actionsreturns', title: 'Devoluciones / Cambios',  icon:'ni-world text-pink', class: '' , disabled: ( this.viewProfile === 'admin' )},
+      { path: '/moneypayment', title: 'Abonos dinero',  icon:'ni-credit-card text-pink', class: '' , disabled:  ( this.viewProfile === 'admin' )},
+      { path: '/inventario', title: 'Inventario',  icon:'ni-books text-pink', class: '' , disabled:  ( this.viewProfile === 'admin' )},
+      { path: '/categoria', title: 'Categoria',  icon:'ni-building text-pink', class: '' , disabled: ( this.viewProfile === 'admin' )},
+      { path: '/provedor', title: 'Provedor',  icon:'ni-world text-pink', class: '' , disabled:( this.viewProfile === 'admin' )},
+      { path: '/perfil', title: 'Roles',  icon:'ni-vector text-pink', class: '' , disabled:  ( this.viewProfile === 'admin' )},
+      { path: '/logs', title: 'Logs',  icon:'ni-ui-04 text-pink', class: '' , disabled: ( this.viewProfile === 'admin' )},
+  ];
+    this.menuItems = ROUTES.filter(menuItem => menuItem.disabled === true );
     this.router.events.subscribe((event) => {
       this.isCollapsed = true;
    });
