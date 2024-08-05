f5f859df0730549481b428ae491d158c49f5a5cf
jecontreras <joseeduar147@gmail.com>
2024-08-05 15:09:32 -0500

    cambios general

--- src/app/pages/orders-web/orders-web.component.ts
+++ src/app/pages/orders-web/orders-web.component.ts
@@ -24,7 +24,7 @@ export class OrdersWebComponent implements OnInit {
   dataFind:any = { txt: "" }
   querys:any = {
     page: 0,
-    limit: 30,
+    limit: 1000000,
     where:{
       estado: 0
     }
